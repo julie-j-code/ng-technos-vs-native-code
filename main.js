46ebcb89d76c9a3ce870e858155d87325f9e502b
Julie J <jeannet.julie@gmail.com>
2022-09-26 10:32:20 +0200

    Séparation add-techno list-technos pour exemple. Non sens UX

--- main.js
+++ main.js
@@ -4,13 +4,12 @@ import technosList from "./modules/technos-list.js";
 
 console.log(window.location.pathname);
 
-if (parameterValue) {
-    renderTechno()
-}else if(window.location.pathname=="/technos-list.html"){
-    technosList.listTechnos(technosList.technos)
-}
-
-document.querySelector("#button")?submit.addEventListener("click", ()=>technosList.addTechno()):""
+// if (parameterValue) {
+//     renderTechno()
+// }else if(window.location.pathname=="/technos-list.html"){
+//     technosList.listTechnos(technosList.technos)
+// }
+parameterValue?renderTechno():window.location.pathname=="/technos-list.html"?technosList.listTechnos(technosList.technos):document.querySelector("#submit")?submit.addEventListener("click", ()=>technosList.addTechno()):""
 
 
 
